f163944308a7080fde37391bd8465ae2419b520a
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2021-03-02 12:32:51 +0100

    ADDING STRING METHODS
    - adding string method to models to get userfriendly description

--- chcalendar/models.py
+++ chcalendar/models.py
@@ -6,9 +6,16 @@ class CHRoom(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateTimeField()
     description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} at {self.date}"
+    
     
 class Host(models.Model):
     name = models.CharField(max_length=250)
     avatar = models.URLField()
     room = models.ForeignKey(CHRoom, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
     
